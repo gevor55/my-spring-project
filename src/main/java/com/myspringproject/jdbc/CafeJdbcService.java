7cf896df4eae861845c85a4119eca5e7d460f7c9
Your Name <galstyan.gevorg93@gmail.com>
2023-04-24 16:13:20 +0400

    a

---
+++ src/main/java/com/myspringproject/jdbc/CafeJdbcService.java
@@ -0,0 +1,50 @@
+package com.myspringproject.jdbc;
+
+import com.myspringproject.model.Cafe;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CafeJdbcService {
+
+    private final JdbcTemplate jdbcTemplate;
+
+
+    public List<Cafe> findAll() {
+
+        String sql = """
+                select * from cafe;
+                """;
+        return jdbcTemplate.query(sql, new CafeRowMapper());
+    }
+
+    public Optional<Cafe> findById(Long id) {
+
+        String sql = """
+                select * from cafe
+                where id = ?
+                """;
+
+        List<Cafe> cafes = jdbcTemplate.query(sql, new CafeRowMapper(), id);
+
+        return cafes.stream()
+                .findFirst();
+    }
+
+    public int insert(Cafe cafe) {
+
+        String sql = """
+                insert into cafe(name, address) 
+                VALUES (?, ?)
+                """;
+        return jdbcTemplate.update(
+                sql, cafe.getName(),
+                cafe.getAddress());
+    }
+
+}
---
+++ src/main/java/com/myspringproject/jdbc/CafeRowMapper.java
@@ -0,0 +1,18 @@
+package com.myspringproject.jdbc;
+
+import com.myspringproject.model.Cafe;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CafeRowMapper implements RowMapper<Cafe> {
+    @Override
+    public Cafe mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new Cafe(
+                rs.getLong("id"),
+                rs.getString("name"),
+                rs.getString("address")
+        );
+    }
+}
